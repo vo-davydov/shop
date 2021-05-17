c562f5f1406af0ac837ec1b5f7421a8066b6ce4d
Davydov <davydovvip@gmail.com>
2021-05-17 07:04:16 +0500

    fix debug

--- src/main/java/com/davydov/shop/logger/RestControllerLogger.java
+++ src/main/java/com/davydov/shop/logger/RestControllerLogger.java
@@ -2,32 +2,36 @@ package com.davydov.shop.logger;
 
 import com.davydov.shop.dto.JwtRequest;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.slf4j.Logger;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class RestControllerLogger {
 
+  private final static Logger logger = LoggerFactory.getLogger(RestControllerLogger.class);
+
   @Around("execution(public * com.davydov.shop.controllers.AuthController.*(..))")
   public void methodProfiling(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-    System.out.println("start profiling");
+    logger.debug("start profiling");
     long begin = System.currentTimeMillis();
     Object[] args = proceedingJoinPoint.getArgs();
     proceedingJoinPoint.proceed();
     if (args.length > 0) {
-      System.out.println("Аргументы:");
+      logger.debug("Arguments:");
       for (Object o : args) {
-        JwtRequest jwtRequest =(JwtRequest)  o;
-        System.out.println(o.toString());
+        JwtRequest jwtRequest = (JwtRequest) o;
+        logger.debug(o.toString());
       }
     }
     long end = System.currentTimeMillis();
     long duration = end - begin;
 
-    System.out.println((MethodSignature) proceedingJoinPoint.getSignature() + " duration: " + duration);
-    System.out.println("end profiling");
+    logger.debug((MethodSignature) proceedingJoinPoint.getSignature() + " duration: " + duration);
+    logger.debug("end profiling");
   }
 }
